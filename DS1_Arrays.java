8cce1f620789adf7f0569772185c356f0cc18f94
Pbiz123 <pgbizoukas@gmail.com>
2022-09-24 11:53:44 -0500

    finished add comments

--- DS1_Arrays.java
+++ DS1_Arrays.java
@@ -7,10 +7,12 @@ public class DS1_Arrays{
         int[] test1 = {4, 1, 1, 10, 10, 5}; //initial length of 6, should output array = 4,1,10,5 with a length of 4
         int[] test2 = {5, 1, 10, 0, 1, 5, 4}; //initial length of 7, should output array =5,1,10,0,4 with a length of 5
         int[] test3 = {1, 2}; //initial length of 2, should output array = 1,2 with a lenght of 2
+        int[] emptyArr = {};
 
         removeDuplicates(test1);
         removeDuplicates(test2);
         removeDuplicates(test3);
+        removeDuplicates(emptyArr);
     }
 
     //remove duplicates method, removes duplicates from given array and outputs the new array and the size of the new array
