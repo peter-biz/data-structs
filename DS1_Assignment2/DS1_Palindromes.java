857938621b15698911a8ff9056908572922fea4a
pbiz <pgbizoukas@gmail.com>
2022-10-06 18:47:55 -0500

    comments, still need more + error checking

--- DS1_Assignment2/DS1_Palindromes.java
+++ DS1_Assignment2/DS1_Palindromes.java
@@ -1,12 +1,13 @@
-import java.util.*;
-
 public class DS1_Palindromes {
     public static void main(String[] args) {
-        //test cases
-        String test1 = "aabbaa"; //output true
+
+        //given test cases
+        String test1 = "aabbaa"; //output: true
         String test2 = "aba";   //output: true
         String test3 = "aabb";  //output: false
 
+	    //my test cases
+	    String myTest1 = ""; //output: true
 
         //run and output test cases
         System.out.println("Input: " + test1 + "\t Output: " + isPalindrome(test1));
@@ -15,23 +16,22 @@ public class DS1_Palindromes {
     }
 
     public static boolean isPalindrome(String s) {
-       // String copy = s;
-        if(s.length() == 0 || s.length() == 1) {
-            return true;
+        if(s.length() == 0 || s.length() == 1) {   //logic for this is that if a string is read the same backwards
+            return true;			   //it's a palindrome, therefore a length of 1 and or 0 is a palindrome
         }
         else {
             for(int i = 0; i < s.length(); i++) {
                 for(int k = s.length() - 1; k > 0; k--) {
-                    if((s.charAt(i) == (s.charAt(k)))) {
-                        return true;
+                    if((s.charAt(i) == (s.charAt(k)))) {   //checks if the value at i is equal to value at k within the string
+                        return true;   //if the value is equal, returns true
                     }
-                    else if(!(s.charAt(i) == s.charAt(k))) {
-                        return false;
+                    else if(!(s.charAt(i) == s.charAt(k))) {   //checks if the value at i is not equal to value at k within the string
+                        return false;   //if the value is not equal, returns false
                     }
                 }
             }
         }
-        return false;
+        return false;  //TODO: probably have to change this, kind of a "default" value if the other statments dont get anywhere...
 
         
     }
