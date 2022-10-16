cfccc0303138e52559eeaca1e52a60ac11f706b6
Pbiz123 <pgbizoukas@gmail.com>
2022-10-16 01:08:21 -0500

    i cri

--- DS1_Assignment2/DS1_TagRemover.java
+++ DS1_Assignment2/DS1_TagRemover.java
@@ -18,14 +18,13 @@ public class DS1_TagRemover {
          * None
          * Tom likes to read books
          */
-
-
         String test2 = "<h1>Nayeem loves counseling</h1>"; 
         String test3 = "<h1><h2>Sanjay has no watch</h2></h1><par>So wait for a while</par>"; 
         String test4 = "<Amee>safat codes like a ninja</amee>"; 
         String test5 = "<h1><par>So wait for a while</par> <Amee>safat codes like a ninja</amee></h1>"; 
         String test6 = "<h1><h2>valid</h2><h3>valid</h3></h1>";
-        String test7 = "<SA premium>Imtiaz has a secret crush</SA premium>"; 
+        String test7 = "<h1>valid</h1>invalid";
+        String test8 = "<SA premium>Imtiaz has a secret crush</SA premium>"; 
 
         // System.out.println("Given text(1): " + test1 + "\nString with tags removed: " + removeTags(test1));
         System.out.println("\nGiven text(2): " + test2 + "\nString with tags removed: " + removeTags(test2)); // output: Nayeem loves counseling
@@ -33,17 +32,14 @@ public class DS1_TagRemover {
         System.out.println("\nGiven text(4): " + test4 + "\nString with tags removed: " + removeTags(test4)); // output: None
         System.out.println("\nGiven text(5): " + test5 + "\nString with tags removed: " + removeTags(test5)); // output: So wait for a while None
         System.out.println("\nGiven text(6): " + test6 + "\nString with tags removed: " + removeTags(test6));  // output: valid valid
-        System.out.println("\nGiven text(7): " + test7 + "\nString with tags removed: " + removeTags(test7) + "\n"); // output: Imtiaz has a secret crush
+        System.out.println("\nGiven text(7): " + test7 + "\nString with tags removed: " + removeTags(test7));  // output: valid None
+        System.out.println("\nGiven text(8): " + test8 + "\nString with tags removed: " + removeTags(test8) + "\n"); // output: Imtiaz has a secret crush
         
     }
 
 
-    /*<h1><h2> hello world </h2></h1> 
-In terms of the Stack problem from Assignment 1, <h1> and <h2> would both be considered as  open “brackets”. And your program will need to keep reading through the input and find it’s  corresponding closing " brackets”. And if it’s found, your program will need to print the String in  between the open and closing tags. After that, that entire substring is out of the equation. 
- */
     public static String removeTags(String s) {
         Stack<String> stack = new Stack<String>();
-
         StringBuilder output = new StringBuilder();
         output.append(" ");
         Boolean match = false;
@@ -72,10 +68,8 @@ In terms of the Stack problem from Assignment 1, <h1> and <h2> would both be con
          * (4): Excpected output: None, current output: safat codes like a ninja   (bad)
          * (5): Excpected output: So wait for a while None, current output: So wait for a while safat codes like a ninja   (bad)
          * (6): Excpected output: valid valid, current output: validvalid   (needs a space in between, ok)
-         * (7): Excpected output: Imtiaz has a secret crush, current output: Imtiaz has a secret crush   (good)
+         * (7): Excpected output: valid None, current output: validinvalid   (bad)
+         * (8): Excpected output: Imtiaz has a secret crush, current output: Imtiaz has a secret crush   (good)
          */
-
-        
     }
-
 }
