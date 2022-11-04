0ce6c26e4286181de8b8c9cfe8ca1b62c7bc3958
Pbiz123 <pgbizoukas@gmail.com>
2022-11-03 21:28:02 -0500

    loser, think it's done now

--- DS1_Assignment3/DS1_MyHashMap.java
+++ DS1_Assignment3/DS1_MyHashMap.java
@@ -2,16 +2,11 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 class DS1_MyHashMap {
- 
-    
     LinkedList<Integer> keys;
     ArrayList<Integer> values;
 
     public static void main(String args[])
     {
-        System.out.println("Output format(key/value pairs) is as follows: \n [Key : Value] , [Key : Value] \n\n");
-
-
         DS1_MyHashMap h = new DS1_MyHashMap();
         h.put(1, 1); // The map is now [[1,1]]
         h.put(2, 2); // The map is now [[1,1], [2,2]]
@@ -21,29 +16,22 @@ class DS1_MyHashMap {
         System.out.println(h.get(2));    // return 1, The map is now [[1,1], [2,1]]
         h.remove(2); // remove the mapping for 2, The map is now [[1,1]]
         System.out.println(h.get(2));    // return -1 (i.e., not found), The map is now [[1,1]]
-
-        
+        //final output should be (1,-1,1,-1)   (passed i think)
     }
 
     public DS1_MyHashMap() {
-        keys = new LinkedList<Integer>();
+        keys = new LinkedList<Integer>(); 
         values = new ArrayList<Integer>();
-
-
     }
 
     //adds a key-value pair to the map
     public void put(int key, int value) {
-        //if the key is already in the map, update existing value
-        
-        //if(indices[key] == null || indices[key] == "8") //TODO logic :0, rethinking the need for an if statment here, at the very least think the condition is wrong
-      
-        if(keys.contains(key))
+        if(keys.contains(key)) //if key already exists, update value
         {
             int index = keys.indexOf(key);
             values.set(index, value);
         }
-        else
+        else  //add key and value
         {
             keys.add(key);
             values.add(value);
@@ -52,39 +40,25 @@ class DS1_MyHashMap {
 
     //returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
     public int get(int key) {
-        if(keys.contains(key))
+        if(keys.contains(key))  //if key exists, return value
         {
-            int index = keys.indexOf(key);
-            int value = values.get(index);
-           
+            int index = keys.indexOf(key);  //get index of key
+            int value = values.get(index);  //get value at index
             return value;
         }
-        else
+        else 
         {
-           
             return -1;
         }
     }
 
     //removes the mapping of the specified value key if this map contains a mapping for the key
     public void remove(int key) {
-        if(keys.contains(key))
+        if(keys.contains(key))  //if key exists, remove key and value
         {
-            int index = keys.indexOf(key);
-            keys.remove(index);
+            int index = keys.indexOf(key); //get index of key
+            keys.remove(index); 
             values.remove(index);
         }
-
-
-    }
-    // public String toString() {
-    //     String s = "";
-    //     for(int i = 0; i < keys.size(); i++)
-    //     {
-    //         s += "[" + keys.get(i) + " : " + values.get(i) + "], ";
-    //     }
-    //     return s;
-    // }
-
-    
+    }    
 }
