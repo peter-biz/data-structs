b45abbd490e9299c51a9561970468f65b2d84557
pbiz <pgbizoukas@gmail.com>
2022-12-03 18:47:21 -0600

    test works but needs to be cleaned up and moved to BFS file

--- DS1_Assignment5/BFS.java
+++ DS1_Assignment5/BFS.java
@@ -13,10 +13,16 @@ public class BFS {
       /*
        * Test cases
        */   //solving for the amount of time the robot can stay at the intial point before having to move due to flooding/wall
-      
+      int[][] test1 = {{0,2,0,0,0,0,0}, {0,0,0,2,2,1,0},{0,2,0,0,1,2,0},{0,0,2,2,2,0,2}, {0,0,0,0,0,0,0}}; //output = 3
+      int[][] test2 = {{0,0,0,0},{0,1,2,0},{0,2,0,0}}; //output = -1
+      int[][] test3 = {{0,0,0}, {2,2,0}, {1,2,0}}; //output = 1000000000
+
+      System.out.println("Test 1: " + maxMin(test1));
+      System.out.println("Test 2: " + maxMin(test2));
+      System.out.println("Test 3: " + maxMin(test3));
    } 
 
-   public class Graph {
+   public static class Graph {
       private int numVertices;
       private LinkedList<Integer> adjLists[];
 
@@ -27,10 +33,72 @@ public class BFS {
          for (int i = 0; i < v; i++) {
             adjLists[i] = new LinkedList<Integer>();
          }
-         //https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
-
-
       }
+
+      void addEdge(int src, int dest) {
+         adjLists[src].add(dest);
+      }
+
+      void BFS(int s) {  
+         boolean visited[] = new boolean[numVertices]; //boolean array to keep track of visited vertices
+         LinkedList<Integer> queue = new LinkedList<Integer>(); //queue to keep track of vertices to be visited
+         visited[s] = true;  //mark the current node as visited and enqueue it
+         queue.add(s);  //add the current node to the queue
+
+         while(queue.size() != 0) {
+            s = queue.poll();  //dequeue a vertex from queue and print it
+            System.out.print(s + " ");  
+
+            Iterator<Integer> i = adjLists[s].listIterator();  //get all adjacent vertices of the dequeued vertex s
+            int next = i.next();  //if a adjacent has not been visited, then mark it visited and enqueue it
+            if(!visited[next]) {  //if the adjacent has not been visited, then mark it visited and enqueue it
+               visited[next] = true;  //mark the adjacent as visited
+               queue.add(next); //add the adjacent to the queue
+            }
+         }
+      }
+      
    }
+
+
+   //converts the given 2d array into a graph
+   public static Graph arrayToGraph(int[][] a) {
+      int rows = a.length;
+      int cols = a[0].length;
+      int numVertices = rows * cols;
+      Graph g = new Graph(numVertices);
+      for(int i = 0; i < rows; i++) {
+         for(int j = 0; j < cols; j++) {
+            if(a[i][j] == 0) {
+               if(i > 0 && a[i-1][j] == 0) {
+                  g.addEdge(i*cols + j, (i-1)*cols + j);
+               }
+               if(i < rows - 1 && a[i+1][j] == 0) {
+                  g.addEdge(i*cols + j, (i+1)*cols + j);
+               }
+               if(j > 0 && a[i][j-1] == 0) {
+                  g.addEdge(i*cols + j, i*cols + j-1);
+               }
+               if(j < cols - 1 && a[i][j+1] == 0) {
+                  g.addEdge(i*cols + j, i*cols + j+1);
+               }
+            }
+         }
+      }
+      return g;
+   }
+
+   public static int maxMin(int[][] a) {
+      Graph g = arrayToGraph(a);
+      g.BFS(0);
+      return 0;
+
+
+      
+      
+      
+   }
+      
+
 }
 
---
+++ DS1_Assignment5/Test.java
@@ -0,0 +1,119 @@
+import java.util.*;
+
+public class Test {
+    public static void main(String args[]) {
+        /*
+         * Test cases
+         */   //solving for the amount of time the robot can stay at the intial point before having to move due to flooding/wall
+        int[][] test1 = {{0,2,0,0,0,0,0}, {0,0,0,2,2,1,0},{0,2,0,0,1,2,0},{0,0,2,2,2,0,2}, {0,0,0,0,0,0,0}}; //output = 3
+        int[][] test2 = {{0,0,0,0},{0,1,2,0},{0,2,0,0}}; //output = -1
+        int[][] test3 = {{0,0,0}, {2,2,0}, {1,2,0}}; //output = 1000000000
+  
+        System.out.println("Test 1: " + maximumMinutes(test1));
+        System.out.println("Test 2: " + maximumMinutes(test2));
+        System.out.println("Test 3: " + maximumMinutes(test3));
+  
+    } 
+
+  
+ 
+    public static int maximumMinutes(int[][] g) {
+      int m = g.length;
+      int n = g[0].length;
+      
+      int[][] times = new int[m][n];
+      for(int[] row : times) {
+          Arrays.fill(row, Integer.MAX_VALUE);
+      }
+      
+      int[] dir = new int[]{-1, 0, 1, 0, -1};
+      `
+    // Queue for running BFS
+      Deque<int[]> floods = new ArrayDeque<>();
+    
+      for(int row = 0; row < m; row++) {
+          for(int col = 0; col < n; col++) {
+              if (g[row][col] == 1) {
+                  times[row][col] = 0;
+                  floods.offer(new int[]{row, col});
+              }
+          }
+      }
+      
+      int time = 1;
+      while(!floods.isEmpty()) {
+          int size = floods.size();
+          
+          for(int i = 0; i < size; i++) {
+              int[] pos = floods.poll();
+              
+              for(int j = 0; j < 4; j++) {
+                  int x = pos[0] + dir[j];
+                  int y = pos[1] + dir[j + 1];
+                  
+                  if (x >= 0 && y >= 0 && x < m && y < n && g[x][y] == 0 && times[x][y] == Integer.MAX_VALUE) {
+                      times[x][y] = time;
+                      floods.offer(new int[]{x, y});
+                  }
+              }
+          }
+          
+          time++;
+      }
+      
+      floods.clear();
+      
+      int ans = Integer.MAX_VALUE;
+      floods.offer(new int[]{0, 0});
+      g[0][0] = 2;
+
+      time = 1;
+      while(!floods.isEmpty() && g[m - 1][n - 1] == 0) {
+          int size = floods.size();
+          
+          int t = Integer.MIN_VALUE;
+          
+          for(int i = 0; i < size && g[m - 1][n - 1] == 0; i++) {
+              int[] pos = floods.poll();
+          
+              for(int j = 0; j < 4 && g[m - 1][n - 1] == 0; j++) {
+                  
+                  int x = pos[0] + dir[j];
+                  int y = pos[1] + dir[j + 1];
+
+                  if (x >= 0 && y >= 0 && x < m && y < n && g[x][y] == 0 && times[x][y] >= time) {
+                      if (x == m - 1 && y == n - 1) {
+                          t = times[x][y] - time;
+                          g[x][y] = 2;
+                          break;
+                      }
+
+                      g[x][y] = 2;
+                      floods.offer(new int[]{x, y});
+                      
+                // if times[x][y] == Integer.MAX_VALUE, flood will never reach this cell and it will contribute maximum wait time
+                      t = Math.max(t, times[x][y] == Integer.MAX_VALUE ? 1000000000 : times[x][y] - time - 1);
+                  }
+              }
+          }
+          
+          ans = Math.min(ans, t);
+          
+          time++;
+      }
+      
+    // You can never reach the safe house
+      if (g[m - 1][n - 1] != 2) {
+          return -1;
+      }
+      
+    // you can reach the safe house but flood can not
+      if (times[m - 1][n - 1] == Integer.MAX_VALUE) {
+          return 1000000000;
+      }
+      
+      return ans == Integer.MAX_VALUE || ans == Integer.MIN_VALUE ? -1 : ans;
+  }
+
+
+    }
